432c20820439cfd4c5e7f9ace51445a4c6e2d4db
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-10-03 00:16:43 +0700

    fix: utils languages and themes

--- lib/constant.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryBlue = Color.fromRGBO(24, 26, 32, 1);
-const secondaryBlue = Color.fromRGBO(31, 34, 44, 1);
-const lightBlue = Color.fromRGBO(84, 104, 255, 1);
-const primaryGray = Color.fromRGBO(247, 247, 247, 1);
-
-const primaryUMM = Color.fromRGBO(38, 42, 52, 1);
--- lib/themes/resources.dart
+++ lib/themes/resources.dart
@@ -9,6 +9,7 @@ class AppColors {
   // primary colors
   Color primaryBg = const Color(0xFFF6F8FC);
   Color background = const Color(0XFF323232);
+
   // secondary colors
   Color hightlight = const Color(0xFFFFE0A2);
   Color darkGrey = const Color(0xFF646464);
