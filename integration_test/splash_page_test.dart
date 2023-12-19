5e8f61d33c4050cb0b4fc74ae9fda1c91c6531be
GeraldiNathan <geraldi.tommysaputra03@gmail.com>
2023-12-20 00:21:11 +0800

    feat(splash): fixing testing

--- integration_test/splash_page_test.dart
+++ integration_test/splash_page_test.dart
@@ -1,16 +1,12 @@
+import 'package:demo_mobile/presentation/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
-import 'package:demo_mobile/main.dart' as app;
-import 'package:demo_mobile/presentation/splash/splash_page.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  testWidgets("SplashPage displays UI elements", (WidgetTester tester) async {
-    app.main();
-
-    await tester.pumpAndSettle();
+  testWidgets('SplashPage UI Test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: SplashPage(),
+    ));
 
     expect(find.byType(SplashPage), findsOneWidget);
 
@@ -20,6 +16,7 @@ void main() {
     );
 
     expect(find.byType(Image), findsOneWidget);
-    expect(find.text('Picvie'), findsOneWidget);
+
+    expect(find.text("Picvie"), findsOneWidget);
   });
 }
